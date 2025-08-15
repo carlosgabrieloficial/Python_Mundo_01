4a825e924e50a32d212cf9c857282bb0044a4665
Carlos Gabriel Sousa <carlos.desenvolvedor.dev.@gmail.com>
2025-08-15 19:10:45 -0300

    Create M01_A01-Sucessor e Antercessor.py

---
+++ M01_A01-Sucessor e Antercessor.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('diga um numero'))                                                  
+print (f"O seu numero é {n},o seu antecessor{n-1} e o seu sucessor{n+1}")
