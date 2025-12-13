c453226f5e56b00a89facd0bdf6db7724c307a3c
Carlos Gabriel Sousa <carlos.desenvolvedor.dev.@gmail.com>
2025-12-13 16:16:36 -0300

    disconsiderar

---
+++ aula.py
@@ -0,0 +1,58 @@
+# numero1 = float(input("Numero 1 :"))
+# numero2 = float(input("Numero 2 :"))
+# soma = numero1 + numero2
+
+
+# print("A soma entre os numeros {} e {} é {}".format(numero1,numero2,soma))
+# print(f"A soma entre os numeros {numero1} e {numero2} é {soma}")
+
+
+
+# dado = input("Digite algum dado :")
+
+# print(type(dado))
+# print(dado.isalnum())
+# print(dado.isalpha())
+
+
+# num1 = int(input("Numero1 :"))
+
+# dobro = num1*2
+# quar  = num1**2
+# cubo  = num1**3
+# raiz  = num1 **(1/2)
+# print(dobro,quar,cubo,raiz)
+
+# nota1 = float(input("nota 1 :"))
+# nota2 = float(input("Nota 2 :"))
+# media = (nota1+nota2) / 2
+
+# print("A nota do aluno é {:.2f}".format(media))
+
+# num = int(input("Numero :"))
+
+# print("=="*10)
+
+# print(f"{1} x {num} = {num}")
+
+
+# num = int(input("Numero :"))
+
+# for i in range(0,11,1):
+#     print(f"{num} x {i} = {num*i}")
+
+
+# real = float(input("digite uma valor :"))
+# # dolar = real / 5.44
+# # print(f"{dolar:.2f}")
+
+# # real = float(input("Digite um valor :"))
+# # print(f"{(real/5.44):.2f}")
+
+# alt = float(input("Altura da sua parede :"))
+# lar = float(input("Qual a largura da parede :"))
+
+# print(f"A sua parede tem a dimesão de {alt}x{lar} e a sua area é de {alt*lar}\nConsidera que a cada litro de tinta pinta 2 metros logo {((alt*lar)/2):.2f} litros")
+
+# produto = float(input("Valor do produto R$"))
+# print(f"O produto custava {produto} e apos o desconto de 5% passou a custar {produto-(produto*0.05):.2f}")
